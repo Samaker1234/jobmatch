3ea9dedd23e80f07edf1fbfca61bf40c6ca7003e
SenyDelamou <samaker1234@users.noreply.github.com>
2026-04-27 12:20:50 +0000

    Increase password_hash length to 255

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ class User(db.Model, UserMixin):
     firstname = db.Column(db.String(50), nullable=False)
     lastname = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=True) # Nullable for Google users
+    password_hash = db.Column(db.String(255), nullable=True) # Nullable for Google users
     google_id = db.Column(db.String(100), unique=True, nullable=True)
     profession = db.Column(db.String(100))
     bio = db.Column(db.Text, nullable=True)
